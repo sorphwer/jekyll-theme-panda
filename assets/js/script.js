3bbc1eca3b8bf81d7093e090e6a1771864e15f98
riino <sorphwer@gmail.com>
2021-07-10 19:07:52 +0800

    script refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
-// ブラウザバックした際にフェードインアニメーションを有効にする（Safari用）
 window.onpageshow = function(event) {
   if (event.persisted) {
     window.location.reload()
   }
 };
 
-// fadein elements on load
 function pageLoaded() {
   $('body').imagesLoaded( function() {
     $('body').addClass('js-loaded');
@@ -25,7 +23,7 @@ function pageTransition() {
       //$('main,footer,.menu').addClass('js-fadeOut');
       $('main,.menu').addClass('js-fadeOut');
       setTimeout(function(){
-        window.location = url;  // 0.6秒後に取得したURLに遷移
+        window.location = url; 
       }, 600);
     }
     return false;
@@ -47,14 +45,12 @@ function fadeinAnime() {
 
 // title animation
 function titleAnimeSetting() {
-  // js-ttl の文字をそれぞれ spanで囲う
   $('.js-ttl').children().addBack().contents().each(function(){
     if (this.nodeType == 3) {
       var $this = $(this);
       $this.replaceWith($this.text().replace(/(\S)/g, '<span>$&</span>'));
     }
   });
-  // js-ttl ごとの spanにそれぞれdata-delayの値を入れる
   $('.js-ttl').each(function(){
     dataDelay = 200;
     $('span', this).each(function(){
@@ -85,7 +81,6 @@ function titleAnime() {
 //   }
 // }
 
-// href="#" であればその id の要素までスクロールする
 // smooth scroll
 function smoothScroll() {
   $('a[href^="#"]').on('click', function(){
@@ -99,7 +94,6 @@ function smoothScroll() {
 
 // header stick
 function headerStick() {
-  // メニューの展開時はヘッダー固定機能をさせない
   if ( !$('body').hasClass('is-menu-active') ) {
     var scrollPos = $(window).scrollTop();
     var headerPos = 20;
@@ -148,76 +142,13 @@ function aboutMissionSlider() {
   });
 }
 
-// artist why slider
-function artistWhySlider() {
-  // トータルのスライド数
-  var $artistWhySlider = $('.js-artist-why-slider');
-  var slideTotal = $('.js-artist-why-slider li').length;
-  $('.artist-why__pager--total').text(slideTotal);
-  // スライド
-  $artistWhySlider.slick({
-    fade: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    cssEase: 'linear',
-    speed: 1000,
-    slidesToShow: 1,
-    infinite: true
-  }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    $('.artist-why__pager--current').text(nextSlide + 1);
-  });
-  // 次のスライドへ移動
-  $('.artist-why__pager a').on('click', function(){
-    $artistWhySlider.slick("slickNext");
-  });
-}
 
-// artist other modal
-function artistOtherModal() {
-  $('.artist-other__list ul li a').on('click', function(){
-    var scrollPos = $(window).scrollTop();
-    $('body').addClass('js-modal-open');
-    $('body').attr('data-scroll',scrollPos);
-    var thisArtist = $(this).attr('data-artist');
-    $('.artist-other__modalOver,.artist-other__modalClose').addClass('js-open');
-    $('#'+thisArtist).addClass('js-open');
-  });
-  $('.artist-other__modalClose a').on('click', function(){
-    var scrollPos = $('body').attr('data-scroll');
-    $('html, body').stop().animate({ scrollTop: scrollPos }, 1);
-    $('body').removeClass('js-modal-open');
-    $('.artist-other__modalOver,.artist-other__modalClose').removeClass('js-open');
-    $('.artist-other__modal').removeClass('js-open');
-  });
-}
-
-// NEWS一覧ページのクロス検索のフォーム
-function newsCrossSearch() {
-  var form = $('.news__select form');
-  var btn = $('.news__selectItem');
-  var btnText = $('.news__selectInner');
-  var pullDown = $('.news__selectContent');
-  var radio = $('.news__selectContentLabel input');
-  // プルダウン開閉
-  btn.on('click', function() {
-    var thisPullDown = $(this).find(pullDown);
-    $(this).find(btnText).toggleClass('js-active');
-    thisPullDown.toggleClass('js-active');
-  });
-  // フォーム送信
-  radio.change(function() { // radioのvalueが変わったら
-    form.submit(); // フォーム送信
-    pullDown.removeClass('js-active');
-  });
-}
 
 // menu for sp
 function sp__menu() {
 
   $('.menuBtn a').on('click', function(){
     $('.menu').addClass('js-active');//important
-    // 背景のコンテンツがスクロールしないようにする
     scrollpos = $(window).scrollTop();
     $('body').attr('data-scroll',scrollpos);
     $('body').addClass('is-menu-active').css({'top': -scrollpos});
@@ -226,7 +157,6 @@ function sp__menu() {
 
   $('.menu_closeBtn').on('click', function(){
     $('.menu').removeClass('js-active');
-    // 背景のコンテンツがスクロールしないようにする
     var scrollPos = $('body').attr('data-scroll');
     $('body').removeClass('is-menu-active').css({'top': '0'});
     window.scrollTo( 0 , scrollPos );
